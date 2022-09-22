172037c8aa3176c5bbd6e385c7bc7ac06b667565
Guillermo <guillermo.arena@gmail.com>
2022-09-21 23:09:08 -0300

    sigo codificando service, controller, repos

---
+++ src/main/java/com/portfolio/gda/Repository/REducacion.java
@@ -0,0 +1,18 @@
+
+package com.portfolio.gda.Repository;
+
+/**
+ *
+ * @author guill
+ */
+import com.portfolio.gda.Entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface REducacion extends JpaRepository<Educacion, Integer>{
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
