2f522ac0ec35dd97201abb6be0c4905115f098f1
Guillermo <guillermo.arena@gmail.com>
2022-09-30 19:57:02 -0300

    update

--- src/main/java/com/portfolio/gda/Controller/CHys.java
+++ src/main/java/com/portfolio/gda/Controller/CHys.java
@@ -29,8 +29,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://frontendgdaP.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendgdap.web.app")
 @RequestMapping("/skill")
 public class CHys {
 
